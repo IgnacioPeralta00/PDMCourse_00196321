a1e75e44e255907ec77ae5ae23404ffcc83d8557
IgnacioPeralta00 <00196321@uca.edu.sv>
2026-03-25 22:45:24 -0600

    feat: Solucion ejercicio4; 'Cuadrantes p.II'

---
+++ Ejercicio4-CuadrantesPII/app/src/main/java/com/fipruno/myapp/MainActivity.kt
@@ -0,0 +1,115 @@
+package com.fipruno.myapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.fipruno.myapp.ui.theme.MyAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                        Quadrants(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun     Quadrants(modifier: Modifier = Modifier) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Row(modifier = Modifier.weight(1f)) {
+            QuadrantCard(
+                title = stringResource(R.string.text_composable_title),
+                description = stringResource(R.string.text_composable_desc),
+                backgroundColor = colorResource(R.color.color1),
+                modifier = Modifier.weight(1f)
+            )
+            QuadrantCard(
+                title = stringResource(R.string.image_composable_title),
+                description = stringResource(R.string.image_composable_desc),
+                backgroundColor = colorResource(R.color.color2),
+                modifier = Modifier.weight(1f)
+            )
+
+                    }
+        Row(modifier = Modifier.weight(1f)) {
+            QuadrantCard(
+                title = stringResource(R.string.row_composable_title),
+                description = stringResource(R.string.row_composable_desc),
+                backgroundColor = colorResource(R.color.color3),
+                modifier = Modifier.weight(1f)
+
+            )
+            QuadrantCard(
+                title = stringResource(R.string.column_composable_title),
+                description = stringResource(R.string.column_composable_desc),
+                backgroundColor = colorResource(R.color.color4),
+                modifier = Modifier.weight(1f)
+            )
+        }
+
+
+    }
+}
+
+@Composable
+fun QuadrantCard(
+    title: String,
+    description: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 13.dp)
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyAppTheme {
+        Quadrants()
+    }
+}
