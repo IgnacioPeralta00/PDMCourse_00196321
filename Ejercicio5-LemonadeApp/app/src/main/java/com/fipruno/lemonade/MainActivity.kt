d46ff003653850e1a7493e51a3c41b474b0b2111
IgnacioPeralta00 <00196321@uca.edu.sv>
2026-03-27 22:42:02 -0600

    feat: Solución al ejercicio; 'LemonadeApp'

--- Ejercicio5-LemonadeApp/app/src/main/java/com/fipruno/lemonade/MainActivity.kt
+++ Ejercicio5-LemonadeApp/app/src/main/java/com/fipruno/lemonade/MainActivity.kt
@@ -4,13 +4,37 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.fipruno.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +44,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             LemonadeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Lemonade(
+                    LemonadeApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,14 +54,90 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Lemonade(modifier: Modifier = Modifier) {
+fun LemonadeApp(modifier: Modifier = Modifier) {
 
+    var tapsOnButton by remember {mutableStateOf(0)}
+    var randomInt by remember { mutableStateOf((2..4).random()) }
+
+    if (tapsOnButton >= 3 + randomInt ) {
+      tapsOnButton = 0;
+      randomInt = (2..4).random();
+    }
+
+    var imageResource = when (tapsOnButton) {
+        0 -> R.drawable.lemon_tree
+        1 -> R.drawable.lemon_squeeze
+        2 -> R.drawable.lemon_drink
+        2 + randomInt -> R.drawable.lemon_restart
+        else -> {
+            R.drawable.lemon_drink
+        }
+    }
+    var stringResource = when (tapsOnButton) {
+        0 -> R.string.lemon_tap
+        1 -> R.string.lemon_squeeze
+        2 -> R.string.lemon_drink
+        2 + randomInt -> R.string.lemon_restart
+        else -> {
+            R.string.lemon_drink
+        }
+    }
+
+
+    Column(modifier.fillMaxSize().background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center)
+    {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(colorResource(R.color.yellow_sunflower))
+                .weight(0.1f)
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = stringResource(R.string.app_name),
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp,
+                color = Color.Black
+            )
+        }
+        Column(
+            modifier = Modifier.weight(1f)
+                .fillMaxWidth()
+                .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center) {
+            Button(
+                onClick = { tapsOnButton++ },
+                shape = RoundedCornerShape(25),
+                colors = ButtonDefaults.buttonColors(colorResource(R.color.mint_pastel))
+                ) {
+                Image(
+                    painter = painterResource(imageResource),
+                    contentDescription = stringResource(R.string.lemon_tree_content_description),
+                    modifier = Modifier
+                        .padding(16.dp)
+
+                )
+            }
+            Text(
+                text = stringResource(stringResource),
+                fontSize = 18.sp,
+                color = Color.Black,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(top = 24.dp, bottom = 8.dp)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LemonadeTheme {
-        Lemonade()
+        LemonadeApp()
     }
 }
