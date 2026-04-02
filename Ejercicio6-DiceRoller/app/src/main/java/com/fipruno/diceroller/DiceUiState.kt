3cbc0b7f184336984cba1f44811cf66114de6905
IgnacioPeralta00 <00196321@uca.edu.sv>
2026-04-01 23:23:27 -0600

    add: data class DiceUiState

---
+++ Ejercicio6-DiceRoller/app/src/main/java/com/fipruno/diceroller/DiceUiState.kt
@@ -0,0 +1,7 @@
+package com.fipruno.diceroller
+
+data class DiceUiState(
+    val diceImage: Int = R.drawable.dice_1,
+    val contentDescription: Int = R.string.dice_1,
+    val diceValue: Int = 1
+)
