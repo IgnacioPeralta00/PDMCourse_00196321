061900d67ef16d2064a0217b9f1a4b0bd435acb5
IgnacioPeralta00 <00196321@uca.edu.sv>
2026-05-25 00:29:56 -0600

    fix: nullable fields on GameDTO

--- EjercicioRepasoParcial2-2/app/src/main/java/com/pdm/fipr/nexusgames/data/api/dto/games/GameDTO.kt
+++ EjercicioRepasoParcial2-2/app/src/main/java/com/pdm/fipr/nexusgames/data/api/dto/games/GameDTO.kt
@@ -10,14 +10,13 @@ data class GameDTO(
     val id: Int,
     val slug: String,
     val name: String,
-    @SerialName("name_original") val nameOriginal: String,
-    val metacritic: Int?,
-    val released: String?,
-    @SerialName("background_image") val backgroundImage: String?,
-    val platforms: List<PlatformContainerDto>?,
-    @SerialName("description_raw") val description: String?,
+    @SerialName("name_original") val nameOriginal: String? = null,
+    val metacritic: Int? = null,
+    val released: String? = null,
+    @SerialName("background_image") val backgroundImage: String? = null,
+    val platforms: List<PlatformContainerDto>? = null,
+    @SerialName("description_raw") val description: String? = null,
 )
-
 // Mapper a modelo
 fun GameDTO.toModel(): Game {
     return Game(
